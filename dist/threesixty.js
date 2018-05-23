4693b4dfc2ae8ac0c748cbb47dfc94f8224257cc
John Kelly <jkelly@carsaver.com>
2018-05-23 09:49:19 -0500

    Upgrade packages to remove security vulnerabilities (and stay up-to-date)

--- dist/threesixty.js
+++ dist/threesixty.js
@@ -2,246 +2,107 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define("threesixty", [], factory);
+		define([], factory);
 	else if(typeof exports === 'object')
 		exports["threesixty"] = factory();
 	else
 		root["threesixty"] = factory();
-})(this, function() {
+})(window, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-
+/******/ 		module.l = true;
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
+/******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-	__webpack_require__(1);
-	module.exports = __webpack_require__(1);
-
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nvar threesixty = function threesixty(container, images, options) {\n  if (!container) {\n    throw new Error('A container argument is required');\n  }\n\n  if (!images) {\n    throw new Error('An images argument is required');\n  }\n\n  var defaults = {\n    interactive: true,\n    currentFrame: 0\n  };\n\n  var o = Object.assign({}, defaults, options);\n  var totalFrames = images.length;\n\n  var mouseX = 0;\n  var oldMouseX = 0;\n\n  //------------------------------------------------------------------------------\n  //\n  //  Initialisation\n  //\n  //------------------------------------------------------------------------------\n\n  var init = function init() {\n    preloadimages(images, start);\n  };\n\n  var preloadimages = function preloadimages(sourceImages, cb) {\n    var total = sourceImages.length;\n    var loaded = 0;\n\n    var onload = function onload() {\n      if (++loaded >= total) cb(finalImages);\n    };\n\n    var finalImages = sourceImages.map(function (item) {\n      var image = new Image();\n      image.src = item;\n      image.onload = onload;\n      image.onerror = onload;\n      image.onabort = onload;\n      image.draggable = false;\n      return image;\n    });\n  };\n\n  var start = function start(loadedImages) {\n    images = loadedImages;\n\n    emptyDomNode(container);\n    container.appendChild(images[o.currentFrame]);\n\n    if (o.interactive) {\n      initListeners();\n    }\n  };\n\n  //------------------------------------------------------------------------------\n  //\n  //  Events\n  //\n  //------------------------------------------------------------------------------\n\n  var initListeners = function initListeners() {\n    container.addEventListener('touchstart', startDrag, { passive: true });\n    container.addEventListener('mousedown', startDrag);\n  };\n\n  var drag = function drag(e) {\n    if (!isTouchEvent(e)) {\n      e.preventDefault();\n    }\n\n    mouseX = e.pageX !== undefined ? e.pageX : e.changedTouches[0].pageX;\n\n    if (mouseX < oldMouseX) {\n      previous();\n    } else if (mouseX > oldMouseX) {\n      next();\n    }\n\n    oldMouseX = mouseX;\n  };\n\n  var startDrag = function startDrag(e) {\n    if (!isTouchEvent(e)) {\n      e.preventDefault();\n    }\n\n    document.addEventListener('touchmove', drag, { passive: true });\n    document.addEventListener('mousemove', drag);\n    document.addEventListener('touchend', stopDrag);\n    document.addEventListener('mouseup', stopDrag);\n  };\n\n  var stopDrag = function stopDrag(e) {\n    if (!isTouchEvent(e)) {\n      e.preventDefault();\n    }\n\n    document.removeEventListener('touchmove', drag);\n    document.removeEventListener('mousemove', drag);\n    document.addEventListener('touchend', stopDrag, { passive: true });\n    document.addEventListener('mouseup', stopDrag);\n  };\n\n  //------------------------------------------------------------------------------\n  //\n  //  Sequence management\n  //\n  //------------------------------------------------------------------------------\n\n  var replaceImage = function replaceImage() {\n    container.replaceChild(images[o.currentFrame], container.childNodes[0]);\n  };\n\n  var previous = function previous() {\n    o.currentFrame--;\n    if (o.currentFrame < 0) o.currentFrame = totalFrames - 1;\n    replaceImage();\n  };\n\n  var next = function next() {\n    o.currentFrame++;\n    if (o.currentFrame === totalFrames) o.currentFrame = 0;\n    replaceImage();\n  };\n\n  var isInteractive = function isInteractive() {\n    return o.interactive;\n  };\n  var getCurrentFrame = function getCurrentFrame() {\n    return o.currentFrame;\n  };\n\n  //------------------------------------------------------------------------------\n  //\n  //  API\n  //\n  //------------------------------------------------------------------------------\n\n  return {\n    init: init,\n    previous: previous,\n    next: next,\n    isInteractive: isInteractive,\n    getCurrentFrame: getCurrentFrame\n  };\n};\n\n//------------------------------------------------------------------------------\n//\n//  Utilities\n//\n//------------------------------------------------------------------------------\n\nvar emptyDomNode = function emptyDomNode(element) {\n  if (element.hasChildNodes()) {\n    while (element.firstChild) {\n      element.removeChild(element.firstChild);\n    }\n  }\n};\n\nvar isTouchEvent = function isTouchEvent(e) {\n  return e.touches;\n};\n\nexports.default = threesixty;\nmodule.exports = exports['default'];\n\n//# sourceURL=webpack://threesixty/./src/index.js?");
 
 /***/ }),
-/* 1 */
-/***/ (function(module, exports) {
 
-	'use strict';
+/***/ 0:
+/*!****************************!*\
+  !*** multi ./src/index.js ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var threesixty = function threesixty(container, images, options) {
-	  if (!container) {
-	    throw new Error('A container argument is required');
-	  }
-
-	  if (!images) {
-	    throw new Error('An images argument is required');
-	  }
-
-	  var defaults = {
-	    interactive: true,
-	    currentFrame: 0
-	  };
-
-	  var o = Object.assign({}, defaults, options);
-	  var totalFrames = images.length;
-
-	  var mouseX = 0;
-	  var oldMouseX = 0;
-
-	  //------------------------------------------------------------------------------
-	  //
-	  //  Initialisation
-	  //
-	  //------------------------------------------------------------------------------
-
-	  var init = function init() {
-	    preloadimages(images, start);
-	  };
-
-	  var preloadimages = function preloadimages(sourceImages, cb) {
-	    var total = sourceImages.length;
-	    var loaded = 0;
-
-	    var onload = function onload() {
-	      if (++loaded >= total) cb(finalImages);
-	    };
-
-	    var finalImages = sourceImages.map(function (item) {
-	      var image = new Image();
-	      image.src = item;
-	      image.onload = onload;
-	      image.onerror = onload;
-	      image.onabort = onload;
-	      image.draggable = false;
-	      return image;
-	    });
-	  };
-
-	  var start = function start(loadedImages) {
-	    images = loadedImages;
-
-	    emptyDomNode(container);
-	    container.appendChild(images[o.currentFrame]);
-
-	    if (o.interactive) {
-	      initListeners();
-	    }
-	  };
-
-	  //------------------------------------------------------------------------------
-	  //
-	  //  Events
-	  //
-	  //------------------------------------------------------------------------------
-
-	  var initListeners = function initListeners() {
-	    container.addEventListener('touchstart', startDrag, { passive: true });
-	    container.addEventListener('mousedown', startDrag);
-	  };
-
-	  var drag = function drag(e) {
-	    if (!isTouchEvent(e)) {
-	      e.preventDefault();
-	    }
-
-	    mouseX = e.pageX !== undefined ? e.pageX : e.changedTouches[0].pageX;
-
-	    if (mouseX < oldMouseX) {
-	      previous();
-	    } else if (mouseX > oldMouseX) {
-	      next();
-	    }
-
-	    oldMouseX = mouseX;
-	  };
-
-	  var startDrag = function startDrag(e) {
-	    if (!isTouchEvent(e)) {
-	      e.preventDefault();
-	    }
-
-	    document.addEventListener('touchmove', drag, { passive: true });
-	    document.addEventListener('mousemove', drag);
-	    document.addEventListener('touchend', stopDrag);
-	    document.addEventListener('mouseup', stopDrag);
-	  };
-
-	  var stopDrag = function stopDrag(e) {
-	    if (!isTouchEvent(e)) {
-	      e.preventDefault();
-	    }
-
-	    document.removeEventListener('touchmove', drag);
-	    document.removeEventListener('mousemove', drag);
-	    document.addEventListener('touchend', stopDrag, { passive: true });
-	    document.addEventListener('mouseup', stopDrag);
-	  };
-
-	  //------------------------------------------------------------------------------
-	  //
-	  //  Sequence management
-	  //
-	  //------------------------------------------------------------------------------
-
-	  var replaceImage = function replaceImage() {
-	    container.replaceChild(images[o.currentFrame], container.childNodes[0]);
-	  };
-
-	  var previous = function previous() {
-	    o.currentFrame--;
-	    if (o.currentFrame < 0) o.currentFrame = totalFrames - 1;
-	    replaceImage();
-	  };
-
-	  var next = function next() {
-	    o.currentFrame++;
-	    if (o.currentFrame === totalFrames) o.currentFrame = 0;
-	    replaceImage();
-	  };
-
-	  var isInteractive = function isInteractive() {
-	    return o.interactive;
-	  };
-	  var getCurrentFrame = function getCurrentFrame() {
-	    return o.currentFrame;
-	  };
-
-	  //------------------------------------------------------------------------------
-	  //
-	  //  API
-	  //
-	  //------------------------------------------------------------------------------
-
-	  return {
-	    init: init,
-	    previous: previous,
-	    next: next,
-	    isInteractive: isInteractive,
-	    getCurrentFrame: getCurrentFrame
-	  };
-	};
-
-	//------------------------------------------------------------------------------
-	//
-	//  Utilities
-	//
-	//------------------------------------------------------------------------------
-
-	var emptyDomNode = function emptyDomNode(element) {
-	  if (element.hasChildNodes()) {
-	    while (element.firstChild) {
-	      element.removeChild(element.firstChild);
-	    }
-	  }
-	};
-
-	var isTouchEvent = function isTouchEvent(e) {
-	  return e.touches;
-	};
-
-	exports.default = threesixty;
-	module.exports = exports['default'];
+eval("module.exports = __webpack_require__(/*! ./src/index.js */\"./src/index.js\");\n\n\n//# sourceURL=webpack://threesixty/multi_./src/index.js?");
 
 /***/ })
-/******/ ])
+
+/******/ });
 });
-;
